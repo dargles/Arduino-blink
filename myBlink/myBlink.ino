527ca1735413ac4632c59e24429c055f20d748fb
dargles <d.argles@gmx.com>
2021-03-18 14:32:45 +0000

    Correct spelling in header

--- myBlink/myBlink.ino
+++ myBlink/myBlink.ino
@@ -1,37 +1,28 @@
-/*
-  Blink
+/**
+ * myBlink
+ * 
+ * This programme is a basic blink programme which I use to check connection to 
+ * new devices. It is also a test mule for working out how to develop Arduino 
+ * programs on GitHub.
+ * @author  David Argles, d.argles@gmx.com
+ * @version 2021-03-18@14:30h
+ */
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
+// Global declarations go here
+#define PIN     LED_BUILTIN // Output pin
+#define PERIOD  1000        // Time between changes in mS
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN, OUTPUT);
+  // Initialise serial output and declare program name
+  Serial.begin(115200);
+  Serial.println("\nProgram: myBlink, 18-03-2021");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(PIN, digitalRead(PIN)||1);   // flip the LED output
+  delay(PERIOD);                     // wait awhile
 }
